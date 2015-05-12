663fd32226a9ecba126c110f393fab1cc182c573
Cristian Caroli <cristiancaroli@gmail.com>
2015-05-12 17:07:42 +0200

    Full ES6(?).

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.get('/print', (req, res) => res.render('print', {
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -34,7 +34,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use((err, req, res, next) => {
     res.status(err.status || 500);
     console.log(app.get('env'));
     res.render('error', {
@@ -46,7 +46,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
@@ -55,7 +55,7 @@ app.use(function(err, req, res, next) {
 });
 
 
-var server = app.listen(process.env.PORT || 3000, function() {
+var server = app.listen(process.env.PORT || 3000, () => {
 
   var host = server.address().address;
   var port = server.address().port;
